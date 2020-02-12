94374605f102b80e36b1e040109cfbd38e695051
Alberto Bujalance <alberto.bujalance@babel.es>
2020-02-12 20:37:14 +0100

    Updates

---
+++ precache-manifest.c92c7ac919f9e14d340133c1bbccece3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333e3d0e4612c8a991cccf9c752a987e",
+    "url": "/quoted-twitter/index.html"
+  },
+  {
+    "revision": "808408520be8b06c0c60",
+    "url": "/quoted-twitter/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "77c361f386a506c00039",
+    "url": "/quoted-twitter/static/js/2.536337a3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quoted-twitter/static/js/2.536337a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808408520be8b06c0c60",
+    "url": "/quoted-twitter/static/js/main.346cc383.chunk.js"
+  },
+  {
+    "revision": "7a2113fbecbdeb497399",
+    "url": "/quoted-twitter/static/js/runtime-main.886c53f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quoted-twitter/static/media/logo.5d5d9eef.svg"
+  }
+]);
